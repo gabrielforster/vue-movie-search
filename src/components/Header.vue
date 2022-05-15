028014beb0617f52e2411a56f80b15dfc5672b0d
gabrielforster <rochafrgabriel@gmail.com>
2022-05-15 20:22:12 -0300

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="nav-bar">
+    <div class="title">
+      <h3>{{ title }}</h3>
+    </div>
+    <div nav-links>
+      <a
+        href="https://github.com/gabrielforster/vue-movie-search"
+        target="_blank"
+        >Github repo</a
+      >
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HeaderNav",
+  props: {
+    title: { type: String, default: "Movie Search" },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.nav-bar {
+  display: flex;
+  justify-content: space-around;
+  margin-block: 1rem;
+}
+</style>
